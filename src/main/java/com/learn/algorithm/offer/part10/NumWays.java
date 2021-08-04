6f6e2bc853199dda32a2d3a8baa25a005541f8c3
zhaozhichao <zhaozhichao@cestc.cn>
2021-08-04 16:54:47 +0800

    青蛙跳台阶问题

--- src/main/java/com/learn/algorithm/offer/part10/NumWays.java
+++ src/main/java/com/learn/algorithm/offer/part10/NumWays.java
@@ -17,7 +17,7 @@ public class NumWays {
     }
 
     private static int numWays(int n) {
-        int a = 0, b = 1, sum;
+        int a = 1, b = 1, sum;
         for (int i = 0; i < n; i++) {
             sum = (a + b) % 1000000007;
             a = b;
